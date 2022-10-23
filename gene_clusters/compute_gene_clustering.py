da9898461a04b531f3fb2fe456a424cf6cb35fb1
Constantin Pape <constantin.pape@embl.de>
2022-10-23 11:26:38 +0200

    Add gene clustering

--- gene_clusters/compute_gene_clustering.py
+++ gene_clusters/compute_gene_clustering.py
@@ -1,5 +1,7 @@
 import os
+from copy import deepcopy
 
+import mobie
 import pandas as pd
 import numpy as np
 import zarr
@@ -13,6 +15,7 @@ def compute_gene_profiles(data_path, gene_table_path, table_folder):
     if os.path.exists(out_table_path):
         return pd.read_csv(out_table_path, sep="\t")
 
+    print(data_path)
     with zarr.open(data_path, "r") as f:
         ds = f["labels/cells"]
         mscales = ds.attrs["multiscales"][0]
@@ -49,7 +52,7 @@ def cluster_genes(gene_profiles, table_folder):
     table = pd.read_csv(table_path, sep="\t")
     col_name = "gene_cluster"
     if col_name in table:
-        return
+        return os.path.basename(table_path)
     data = table.drop(columns="label_id").values
     # perform simple k-means clustering
     clustering = KMeans(n_clusters=8)
@@ -57,28 +60,54 @@ def cluster_genes(gene_profiles, table_folder):
     cluster_ids = clustering.predict(data)
     table[col_name] = cluster_ids
     table.to_csv(table_path, sep="\t", index=False)
+    return os.path.basename(table_path)
 
 
 def compute_gene_clustering(label_path, gene_table, cell_table_folder):
     gene_profiles = compute_gene_profiles(label_path, gene_table, cell_table_folder)
-    cluster_genes(gene_profiles, cell_table_folder)
+    return cluster_genes(gene_profiles, cell_table_folder)
 
 
-# TODO
+def add_example_view(ds_folder, pos_name, table_name, view_name):
+    metadata = mobie.metadata.read_dataset_metadata(ds_folder)
+    view = deepcopy(metadata["views"][pos_name])
+
+    displays = view["sourceDisplays"]
+    new_displays = []
+    for display in displays:
+        display_type, display_data = next(iter(display.items()))
+        if display_type == "segmentationDisplay":
+            display_data["additionalTables"] = [table_name]
+            display_data["visible"] = True
+            display_data["showTable"] = True
+            display_data["colorByColumn"] = "gene_cluster"
+            display = {display_type: display_data}
+        new_displays.append(display)
+
+    view["uiSelectionGroup"] = "bookmarks"
+    metadata["views"][view_name] = view
+
+    mobie.metadata.write_dataset_metadata(ds_folder, metadata)
+
+
 def main():
-    root = ""
-    ds_name = ""
+    root = "/g/kreshuk/data/marioni/shila/mouse-atlas-2020/ngff"
+    ds_name = "embryo3"
     ds_folder = os.path.join(root, ds_name)
 
-    label_path = os.path.join(ds_folder, "images", "ome-zarr", "MMStack_Pos42.ome.zarr")
-    gene_table_path = os.path.join(
-        ds_folder, "tables", "", "default.tsv"
-    )
+    pos_name = "MMStack_Pos0"
 
-    cell_table_folder = os.path.join(
-        ds_folder, "tables", "", "defaulttsv"
+    label_path = os.path.join(ds_folder, f"{pos_name}.ome.zarr")
+    assert os.path.exists(label_path), label_path
+    gene_table_path = os.path.join(
+        ds_folder, "tables", f"{pos_name}_genes", "default.tsv"
     )
-    compute_gene_clustering(label_path, gene_table_path, cell_table_folder)
+    assert os.path.exists(gene_table_path), gene_table_path
+
+    cell_table_folder = os.path.join(ds_folder, "tables", f"{pos_name}_cells")
+    assert os.path.exists(cell_table_folder), cell_table_folder
+    table_name = compute_gene_clustering(label_path, gene_table_path, cell_table_folder)
+    add_example_view(ds_folder, pos_name, table_name, view_name="gene-clustering-example")
 
 
 if __name__ == "__main__":
